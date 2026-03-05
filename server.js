7cf91b0a2c82855bba8889d4e43f958c9a81529c
atoniezabardzo <78559462+atoniezabardzo@users.noreply.github.com>
2026-03-06 00:51:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -211,7 +211,7 @@ async function sendToDiscord(payload) {
                      descriptionLines.join('\n') + 
                      "\n```",
         footer: {
-            text: `Scan Time: ${new Date().toLocaleTimeString()}`
+            text: `Scan Time: ${new Date().toLocaleTimeString('de-DE', { timeZone: 'Europe/Berlin' })}`
         }
     };
 
